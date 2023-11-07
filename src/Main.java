b8c0a768727cc2a687978687715c941b42270554
Ewan132 <ewan132@yandex.ru>
2023-11-07 19:55:12 +0300

    Create hwQAJava2_1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 13_676;
+        int bonusMile = ticketPrice/20;
+
+        System.out.println(bonusMile);
+    }
+}
